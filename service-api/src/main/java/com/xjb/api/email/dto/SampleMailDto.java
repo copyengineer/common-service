ebadbcce6937aa86d7f864d46bad01b3849c5598
xinjibin <2236492050@qq.com>
2022-03-24 19:55:07 +0800

    初始化项目

---
+++ service-api/src/main/java/com/xjb/api/email/dto/SampleMailDto.java
@@ -0,0 +1,18 @@
+package com.xjb.api.email.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author 辛集斌
+ * @date 2022/3/24
+ * 发送简单邮件入参
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SampleMailDto {
+}
